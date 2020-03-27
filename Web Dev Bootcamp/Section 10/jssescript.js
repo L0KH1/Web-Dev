59e2c001074e9b72a06d29b6664ea3e23039fa0d
Loki <43547464+L0KH1@users.noreply.github.com>
2020-03-26 20:17:47 -0400

    lenovo fix attempt 2

---
+++ Web Dev Bootcamp/Section 10/jssescript.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+alert("Ahoy " + firstName + " " + lastName +".");
+alert("I've never met someone who is " + age + " years old!");
