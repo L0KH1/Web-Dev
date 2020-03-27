abbaab9318ee49ae8a6781aef70fc5ff87b07627
Loki <43547464+L0KH1@users.noreply.github.com>
2020-03-26 22:32:01 -0400

    portfolio v1 done

--- gc-portfolio.js
+++ gc-portfolio.js
@@ -1,17 +1,37 @@
-new Chart(document.getElementById("pie-chart"), {
+Chart.defaults.global.defaultFontFamily = "'Playfair Display', 'serif'";
+
+new Chart(document.getElementById("total-allocation"), {
     type: 'pie',
     data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      labels: ["Equities", "Options", "Currency", "Futures", "Cash"],
       datasets: [{
         label: "Population (millions)",
         backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,734,784,433]
+        data: [5267,2478,734,784,433]
       }]
     },
     options: {
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
+        legend: {
+            labels: {
+                fontColor: 'white'
+            }}}
+});
+
+
+new Chart(document.getElementById("by-sector"), {
+    type: 'pie',
+    data: {
+      labels: ["Communication Services", "Consumer Discretionary", "Consumer Staples", "Energy", "Financials", "Healthcare", 
+               "Industrials", "Technology", "Materials", "Real Estate", "Utilities"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#fff"],
+        data: [500,500,500,500,500,500,500,500,500,500,500]
+      }]
+    },
+    options: {
+        legend: {
+            labels: {
+                fontColor: 'white'
+            }}}
 });
